dcc4ca7b51a5c58d0bf680621a46f5c24bd9b490
minyoungshin <mins6649@gmail.com>
2023-02-01 13:38:41 -0500

    .find() and array iterator methods

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+/*
+//ARRAY INTERATOR METHOD
+function superbowlWin(array) {
+    for (const object of array){
+      if(object.result === "W"){
+        return object.year
+      }
+    }
+  }
+  superbowlWin();
+*/
+
+//.FIND()
+function superbowlWin(record){
+    const object = record.find(element => element.result === "W")
+    if (object === undefined){
+        return undefined
+    }
+    return object.year
+}
+superbowlwin();
